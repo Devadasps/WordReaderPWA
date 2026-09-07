5438eb33ca704e5e2d194b60b9f9f9e7e417f56c
Devadas P S <devadaspanokkils@gmail.com>
2026-09-07 21:58:39 +0530

    Initial commit for DocTools PWA

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'doctools-v3';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/img2pdf.html',
+    '/pdfviewer.html',
+    '/manifest.json',
+    '/icon.png',
+    'https://cdnjs.cloudflare.com/ajax/libs/mammoth/1.6.0/mammoth.browser.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            }));
+        })
+    );
+    self.clients.claim(); // Forces the app to work offline immediately
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((response) => response || fetch(e.request))
+    );
+});
